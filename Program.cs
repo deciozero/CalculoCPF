63a7e5fdbb93fbdeabe1113fa63d1822eba89221
Edecio <edeciogomes@globomail.com>
2018-04-24 18:51:08 -0300

    Atividade De Programação

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace CalculoCPF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           string cpfCliente;
+           string cpf9;
+           string cpf10;
+           int contagem1=10;
+           int contagem2=11;
+           int resto=0;
+           int rs1 = 0;
+           int rs2 = 0;
+
+           cpfCliente = "83205608615";
+           cpf9 = cpfCliente.Substring(0,9);
+           for(int i=0; i < cpf9.Length;i++)
+
+           {
+               rs1 += int.Parse(cpf9[i].ToString())*contagem1;
+               contagem1--;
+           }
+           resto = rs1%11;
+           if(resto < 2)
+           cpf10 = cpf9+0;
+           else
+           cpf10 = cpf9+(11-resto);
+
+           for(int i=0; i < cpf10.Length;i++)
+           
+           {
+               rs2 += int.Parse(cpf10[i].ToString())*contagem2;
+               contagem2--;
+           }
+               resto = rs2%11;
+               if(resto < 2)
+               Console.WriteLine(cpf10+0);
+               else
+               Console.WriteLine(cpf10+(11-resto));
+        }
+    }   
+}
+
+               
+               
+        
+
+           
+           
+
+        
+    
+
